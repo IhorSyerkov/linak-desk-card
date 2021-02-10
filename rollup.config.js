801cabbb8be64f991cc78d21f0b79e55ad521bd1
Ihor Syerkov <serkov.ihor@gmail.com>
2021-02-10 20:33:14 +0100

    first release

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,14 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
+import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import image from '@rollup/plugin-image';
+import del from 'rollup-plugin-delete'
 
 const dev = process.env.ROLLUP_WATCH;
-
-const serveopts = {
-  contentBase: ['./dist'],
-  host: '0.0.0.0',
-  port: 5000,
-  allowCrossOrigin: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-};
-
-const plugins = [
-  nodeResolve({}),
-  commonjs(),
-  typescript(),
-  json(),
-  babel({
-    exclude: 'node_modules/**',
-  }),
-  dev && serve(serveopts),
-  !dev && terser(),
-];
 
 export default [
   {
@@ -38,6 +17,29 @@ export default [
       dir: 'dist',
       format: 'es',
     },
-    plugins: [...plugins],
+    plugins: [
+      del({ targets: 'dist/*' }),
+      minifyHTML(),
+      nodeResolve({}),
+      typescript(),
+      json(),
+      image(),
+      alias({
+        entries: [{
+          find: 'lit-html/lib/shady-render.js',
+          replacement: 'node_modules/lit-html/lit-html.js'
+        }]
+      }),
+      terser({ output: { comments: false } }),
+      dev && serve({
+        contentBase: ['./dist'],
+        host: '0.0.0.0',
+        port: 5000,
+        allowCrossOrigin: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }),
+    ],
   },
 ];
