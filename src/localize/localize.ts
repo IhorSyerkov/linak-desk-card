cb5a4d4a57192bf1ba0f8490aeb5c93a503547b1
padawarmik <43675819+padawarmik@users.noreply.github.com>
2022-04-27 12:27:01 +0200

    feat: Add polish language (#17)

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -2,6 +2,7 @@ import * as en from './languages/en.json';
 import * as uk from './languages/uk.json';
 import * as de from './languages/de.json';
 import * as fr from './languages/fr.json';
+import * as pl from './languages/pl.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
@@ -9,6 +10,7 @@ const languages: any = {
   uk,
   de,
   fr,
+  pl,
 };
 
 export function localize(string: string, search = '', replace = ''): string {
